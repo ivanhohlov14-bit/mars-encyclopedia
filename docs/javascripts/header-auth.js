4eca1ad65515e134ec0aa13ddb446e6933e56fa2
Mnemis <ivanhohlov14@yandex.ru>
2026-08-21 22:51:24 +0400

    Update header-auth.js

--- docs/javascripts/header-auth.js
+++ docs/javascripts/header-auth.js
@@ -2,7 +2,7 @@
 
 (function() {
     // --- Данные Supabase ---
-    const SUPABASE_URL = "https://ncytbgbjfoqmmgfygz.supabase.co";
+   const SUPABASE_URL = "https://ncytbgbzfjfoqmmgfygz.supabase.co";
     const SUPABASE_KEY = "sb_publishable_v5qJYCi85UdrUsz0tAOohQ_0wWdMR3D";
 
     // --- Загружаем Supabase ---

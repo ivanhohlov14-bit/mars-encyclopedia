797b0e47745aa03138d4e10c3ba24084fe7ecdb2
Mnemis <ivanhohlov14@yandex.ru>
2026-08-24 15:16:59 +0400

    Update comments-loader.js

--- docs/javascripts/comments-loader.js
+++ docs/javascripts/comments-loader.js
@@ -14,6 +14,7 @@
             '/register/',
             '/stats/',
             '/profile-view/',
+            '/moderator/',  
             '/license/',
             '/support/',
             '/start-here/',
